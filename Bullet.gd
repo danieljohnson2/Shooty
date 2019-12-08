ed4e787d793b39b4484f577a12e015782f527126
Dan Johnson <danj@fastmail.com>
2019-12-08 14:58:34 -0500

    Fade the boom out as it expands

--- Bullet.gd
+++ Bullet.gd
@@ -4,14 +4,18 @@ export var speed = 300
 export var duration = 1.5
 
 var explodyness = 0
+const max_explodyness = 1.5
 
 func _process(delta):
 	self.position.y -= delta * speed
 	
-	if explodyness > 1.5:
+	if explodyness > max_explodyness:
 		self.queue_free()
 	if explodyness > 0:
-		$"Boom".scale = Vector2(explodyness, explodyness)
+		var boom = $"Boom"
+		var fraction = (max_explodyness - explodyness) / max_explodyness
+		boom.scale = Vector2(explodyness, explodyness)
+		boom.modulate = Color(1, fraction, fraction, fraction) 
 		explodyness *= 1.1
 	else:
 		duration -= delta
