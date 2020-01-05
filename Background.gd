c52dd07a3a8c1732ca47d03afc2438b52825bb1c
Dan Johnson <danj@fastmail.com>
2020-01-05 14:55:45 -0500

    Use a group to control scolling

--- Background.gd
+++ Background.gd
@@ -3,13 +3,17 @@ extends Sprite
 export var speed = 40
 
 func _process(delta):
-	for idx in range(0, self.get_parent().get_child_count()):
-		var n = self.get_parent().get_child(idx)
+	# These things are 'part of the paper' and move downwards with it
+	var on_paper = self.get_tree().get_nodes_in_group("On Paper")
+	
+	for n in on_paper:
 		n.position.y += speed * delta
 	
-		if n == self:
-			while self.position.y >= 0:
-				self.position.y -= self.texture.get_height() * self.scale.y
-		elif n.position.y > 620:
+	# The background gets reset so it never falls over the bottom
+	while self.position.y >= 0:
+		self.position.y -= self.texture.get_height() * self.scale.y
+
+	# Everyone else is not so lucky!
+	for n in on_paper:
+		if n.position.y > 620:
 			n.queue_free()
-	pass
