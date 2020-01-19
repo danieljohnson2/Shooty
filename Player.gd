b9a02c87f50526bc73352db5832eb1b42668cc69
Dan Johnson <danj@fastmail.com>
2020-01-19 09:55:33 -0500

    Correct player facing; can't face down but no longer flips left and right

--- Player.gd
+++ Player.gd
@@ -2,15 +2,23 @@ extends RigidBody2D
 
 export var speed = 200
 
+var right_angle = PI/2
+var deadzone_angle = PI/20
 
 onready var holeTemplate = preload("res://Hole.tscn")
 
 func _process(delta):
-	var rot = self.rotation + self.get_angle_to(get_global_mouse_position()) + PI/2
+	var rot = get_global_mouse_position().angle_to_point(self.global_position) + PI/2
 	
-	self.rotation = max(-PI/2, min(rot, PI/2))
-	
-	self.position.x +=  (self.rotation) * delta * 200
+	if rot < PI-deadzone_angle or rot > PI+deadzone_angle:
+		# Don't allow player to face downwards. We get angles from
+		# -PI/2 to 3/2*PI for some reason.
+		if rot > PI: rot = -right_angle
+		elif rot > PI/2: rot = right_angle
+		elif rot < -PI/2: rot = right_angle
+		
+		self.rotation = rot
+		self.position.x += rot * delta * 200
 		
 	if Input.is_action_just_pressed("ui_shoot"):
 		var hole = holeTemplate.instance()
