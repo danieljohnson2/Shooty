a29d87dfa9b3f92845aa5995b1eaca612d5b5da7
Dan Johnson <danj@fastmail.com>
2020-01-19 10:10:26 -0500

    Awkward! Make the player an Area2D so it has no physics interactions; but we use a different method to detect area-to-area collisions

--- Hole.gd
+++ Hole.gd
@@ -12,9 +12,10 @@ func _process(delta):
 	rotation = (1 - expansion) * PI/2
 	
 func _on_Hole_body_entered(body):
-	if body.is_in_group("Player"):
-		body.queue_free()
-		
 	if body.is_in_group("Enemy"):
 		body.queue_free()
 		$"../Background".speed += 1
+
+func _on_Hole_area_entered(area):
+	if area.is_in_group("Player"):
+		area.queue_free()
