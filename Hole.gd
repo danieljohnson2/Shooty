04cca246da3560437448d481717db2036de6ac36
Dan Johnson <danj@fastmail.com>
2020-01-19 09:18:55 -0500

    Animate holes as the appear. Scaling the root node seems to work on an Area2D

--- Hole.gd
+++ Hole.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
+export var growth_speed = 0.25
+
+func _ready():
+	scale = Vector2(0.125, 0.125)
+	
+func _process(delta):
+	var expansion = scale.x
+	expansion = min(1, expansion + growth_speed * delta)	
+	scale = Vector2(expansion, expansion)
+	rotation = (1 - expansion) * PI/2
+	
 func _on_Hole_body_entered(body):
 	if body.is_in_group("Player"):
 		body.queue_free()
